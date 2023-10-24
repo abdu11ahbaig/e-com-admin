e08c09a41f39b895532a6696e2412913b5378e1b
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-24 10:52:11 +0500

    add settings routes to dashbard

--- app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
@@ -23,8 +23,8 @@ import {
 import { Separator } from "@/components/ui/separator"
 import { Heading } from "@/components/ui/heading"
 import { AlertModal } from "@/components/modals/alert.modal"
-// import { ApiAlert } from "@/components/ui/api-alert"
-// import { useOrigin } from "@/hooks/use-origin"
+import { ApiAlert } from "@/components/ui/api-alert"
+import { useOrigin } from "@/hooks/use-origin"
 
 const formSchema = z.object({
   name: z.string().min(2),
@@ -41,7 +41,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
   const params = useParams();
   const router = useRouter();
-  // const origin = useOrigin();
+  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -122,11 +122,11 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
         </form>
       </Form>
       <Separator />
-      {/* <ApiAlert
+      <ApiAlert
         title="NEXT_PUBLIC_API_URL"
         variant="public"
         description={`${origin}/api/${params.storeId}`}
-      /> */}
+      />
     </>
   );
 };
